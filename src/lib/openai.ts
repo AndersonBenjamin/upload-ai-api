872575c9b42e25dd4deaab719977e392207d0cfe
AndersonBenjamin <anderson.targino@dnkinfotelecom.com.br>
2023-09-20 17:37:52 -0300

    Projeto concluído

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -2,5 +2,6 @@ import 'dotenv/config'
 import { OpenAI } from 'openai'
 
 export const openai = new OpenAI({
-    apiKey: process.env.OPENAI_KEY
+    //apiKey: process.env.OPENAI_KEY
+    apiKey: "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"
 })
--- src/routes/generate_ai_completion.ts
+++ src/routes/generate_ai_completion.ts
@@ -1,39 +1,55 @@
-import { FastifyInstance } from "fastify";
-import { createReadStream } from 'node:fs'
+import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
-import { prisma } from "../lib/prisma";
-import { openai } from "../lib/openai";
+import { streamToResponse, OpenAIStream } from 'ai'
+import { prisma } from '../lib/prisma'
+import { openai } from '../lib/openai'
 
 export async function generateAICompletionRoute(app: FastifyInstance) {
-    app.post('/ai/complete', async (req, reply) => {
-        const bodySchema = z.object({
-            videoId: z.string().uuid(),
-            template: z.string(),
-            temperature: z.number().min(0).max(1).default(0.5),
-        })
-
-        const { videoId, template, temperature } = bodySchema.parse(req.body)
-
-        const video = await prisma.video.findUniqueOrThrow({
-            where: {
-                id: videoId,
-            }
-        })
-
-        if (!video.transcription) {
-            return reply.status(400).send({error: 'Video transcription was not generated yet.'})
-        }
-
-        const promptMessage = template.replace('{transcription}', video.transcription)
-
-        const response = await openai.chat.completions.create({
-            model: 'gpt-3.5-turbo',
-            temperature,
-            messages: [
-                {role: 'user', content: promptMessage}
-            ],
-        })
-
-        return response
+  app.post('/ai/complete', async (req, reply) => {
+    const bodySchema = z.object({
+      videoId: z.string().uuid(),
+      prompt: z.string(),
+      temperature: z.number().min(0).max(1).default(0.5),
     })
+
+    const { videoId, prompt, temperature } = bodySchema.parse(req.body)
+
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoId,
+      }
+    })
+
+    if (!video.transcription) {
+      return reply.status(400).send({ error: 'Video transcription was not generated yet.' })
+    }
+
+   
+
+    const promptMessage = prompt.replace('{transcription}', video.transcription)
+
+    
+
+
+
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo-16k',
+      temperature,
+      messages: [
+        { role: 'user', content: promptMessage },
+      ],
+    })
+
+    return response?.choices[0]?.message?.content
+
+    // const stream = OpenAIStream(response)
+
+    // streamToResponse(stream, reply.raw, { // raw retorna a referência da resposta nativa do node
+    //   // por estar usando a resposta interna do node, é necessário fazer a configuração do cors de forma manual
+    //   headers: {
+    //     'Access-Control-Allow-Origin': '*',
+    //     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    //   },
+    // })
+  })
 }
